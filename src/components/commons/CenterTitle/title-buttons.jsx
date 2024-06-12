b13c7897a4f5abb6393a92424b709b8ae64288f8
Hossam <hossam.zerouali@estudiantat.upc.edu>
2024-06-12 14:27:05 +0200

    botones con redireccion y resize

--- src/components/commons/CenterTitle/title-buttons.jsx
+++ src/components/commons/CenterTitle/title-buttons.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-import './title-buttons.css'
+import React from 'react';
+import './title-buttons.css';
 
 const TitleButtons = () => {
+    const handleDownloadCVClick = () => {
+        window.location.href = 'https://drive.google.com/uc?export=download&id=1URSXA1rwHUBrXanpeGGJK85fDJ8jBJJL';
+    };
+
+    const handleLinkedInClick = () => {
+        window.location.href = 'https://www.linkedin.com/in/hossam-zerouali-385643305/';
+    };
+
+    const handleGithubClick = () => {
+        window.location.href = 'https://github.com/HossamZerouali';
+    };
+
     return (
         <div className="container">
             <div className='title-desc'>
@@ -15,12 +27,18 @@ const TitleButtons = () => {
                 </p>
             </div>
             <div className="buttons-container">
-                <button className="btn">Portfolio</button>
-                <button className="btn">About Me</button>
-                <button className="btn">Contact</button>
+                <button className="btn" onClick={handleDownloadCVClick}>
+                    <img className='icon' src="https://api.iconify.design/simple-line-icons:cloud-download.svg?color=white" alt="CV Icon" /> Download CV
+                </button>
+                <button className="btn" onClick={handleLinkedInClick}>
+                    <img className='icon' src="https://api.iconify.design/simple-line-icons:social-linkedin.svg?color=white"  alt="LinkedIn Icon" /> LinkedIn
+                </button>
+                <button className="btn" onClick={handleGithubClick}>
+                    <img className='icon' src="https://api.iconify.design/simple-line-icons:social-github.svg?color=white" alt="Github Icon" /> Github
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
-export default TitleButtons
+export default TitleButtons;
